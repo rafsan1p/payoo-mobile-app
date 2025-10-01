c0f705d959762aa463ca6523c5756fd7b6b2bf9b
rafsan1p <rubayetrafsan605@gmail.com>
2025-10-01 23:38:48 +0600

    add money section added

---
+++ home.js
@@ -0,0 +1,26 @@
+const validPin = 1234
+
+document.getElementById('add-money-btn')
+    .addEventListener('click', function(e){
+        e.preventDefault();
+        const bank = document.getElementById('bank').value;
+        const accountNumber = document.getElementById('account-number').value;
+        const amount = parseInt(document.getElementById('add-amount').value);
+        const pin = parseInt(document.getElementById('add-pin').value);
+
+        const availableBalance = parseInt(document.getElementById('available-balance').innerText);
+
+        if(accountNumber.length < 11){
+            alert("please provide valid account number");
+            return;
+        }
+        if(pin !== validPin){
+            alert("please provide valid pin number");
+            return;
+        }
+
+        const total = amount + availableBalance;
+
+        document.getElementById('available-balance').innerText = total;
+
+    }) 
